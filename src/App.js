821667ecd3083db898b692e65446818f83ef71c5
Ujjaval-gamit <72432589+Ujjaval-gamit@users.noreply.github.com>
2021-02-05 18:56:08 +0530

    to

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./cmp/Header"
+import React, { useState, useEffect } from 'react'
 
-function App() {
+import { Tasks } from "./cmp/Tasks"
+import { AddTask  } from './cmp/AddTask';
+import { footer } from "./cmp/Footer";
+
+  const App = (props)=> {
+  const [showAddTask, setShowAddTask]=useState(false)
+  const[ tasks, setTasks]= useState([])
+
+  useEffect(() => {
+       const getTask =async () =>{
+         const taskFromServer =await fetchTasks()
+         setTasks(taskFromServer)
+       }
+     getTask()
+  } ,[])
+
+//Fetch Task
+  const fetchTasks =async ()=> {
+    const res=await fetch('http://localhost:5000/tasks')
+    const data=await res.json()
+   // console.log(data)
+
+    return data
+  }
+
+  //Fetch Task For Reminder
+  const fetchTask =async (id)=> {
+    const res=await fetch(`http://localhost:5000/tasks/${id}`)
+    const data=await res.json()
+   // console.log(data)
+
+    return data
+  }
+//ADD
+const addTask = async (task) =>{
+  const res=await fetch('http://localhost:5000/tasks',
+  {
+    method:'POST',
+    headers:{
+       'Content-type' :'application/json'
+    },
+     body:JSON.stringify(task)
+  })
+      const data= await res.json()
+
+      setTasks([...tasks, data])
+ 
+//   const id=Math.floor(Math.random()*100)+1
+//   const newTask={id, ...task }
+//   setTasks([...tasks,newTask])
+
+// }
+}
+ //delete 
+ const DeleteTask = async (id) =>{
+  await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'DELETE'
+  })
+  setTasks(tasks.filter((task)=>task.id !==id))
+ }
+  // Toggel Reminder
+  const toggleReminder = async (id)=> {
+    const taskToToggle =await fetchTask(id)
+    const updTask={...taskToToggle ,
+    reminder: !taskToToggle.reminder}
+    
+    const res =await fetch(`http://localhost:5000/tasks/${id}`,{
+    method:'PUT',
+    headers:{
+      'Content-type':'application/json',
+    },
+    body: JSON.stringify(updTask),
+  }
+    )
+   const data=await res.json()
+    setTasks(
+      tasks.map((task)=> 
+      task.id === id? {...task, reminder:
+      data.reminder }:task
+    ))   
+    console.log(id)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header
+       onAdd={() =>setShowAddTask
+      (!showAddTask) }
+       showAdd={showAddTask} />
+
+     {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length >0 ?
+      <Tasks tasks={tasks}  onDelete={DeleteTask}
+       onToggle={toggleReminder}/>:
+      'No Task  Here'
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+ export default App;
